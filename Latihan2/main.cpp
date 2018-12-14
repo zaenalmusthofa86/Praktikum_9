7607131f094533818839de14ef298f56886530da
Zaenal Mustofa <zaenalmusthofa97@gmail.com>
2018-12-14 22:25:41 +0700

    Program menentukan modus dari n bilangan bulat

---
+++ Latihan2/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+class HitungStatistik
+{
+friend ostream& operator<<(ostream&, HitungStatistik&);
+friend istream& operator>>(istream&, HitungStatistik&);
+public:
+HitungStatistik();
+void hitung_modus();
+private:
+void maksimum();
+void frekuensi();
+int maks, item;
+int n;
+int A[20];
+int f[11];
+};
+
+HitungStatistik::HitungStatistik()
+{
+     for (int i=0; i<20; i++) f[i] = 0;
+}
+
+istream& operator>>(istream& in, HitungStatistik& a)
+{
+cout << "Banyaknya data : ";
+cin >> a.n;
+cout << endl;
+for (int i = 0; i < a.n; i++)
+{
+cout << "Data ke- : " << i+1 << " = ";
+cin >> a.A[i];
+cout << endl;
+}
+return in;
+}
+
+void HitungStatistik::maksimum()
+{
+maks = f[0];
+item = 1;
+for (int i=0; i<n; i++)
+if (f[i] > maks)
+{
+maks = f[i];
+item = i;
+}
+cout << "Modus : " << item << endl;
+cout << endl;
+}
+
+void HitungStatistik::frekuensi()
+{
+for (int i=1; i<n; i++) ++f[A[i]];
+}
+
+void HitungStatistik::hitung_modus()
+{
+cout << "===================" << endl;
+cout << "|Frekuensi running|" << endl;
+cout << "===================" << endl;
+cout << endl;
+frekuensi();
+maksimum();
+}
+
+ostream& operator<<(ostream& out, HitungStatistik& a)
+{
+cout << "============" << endl;
+cout << "|Loading...|" << endl;
+cout << "============" << endl;
+cout << endl;
+a.hitung_modus();
+cout << "Nilai Modus : " << a.item << endl;
+cout << endl;
+return out;
+}
+
+main()
+{
+HitungStatistik run;
+cin >> run;
+cout << run;
+return 0;
+}
